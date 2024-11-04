09822b1550f0ba0902981397c636e71427f868c2
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2024-11-04 20:37:42 -0300

    Update 05 aula - estrutura de repeticao (while and do while).cpp.cpp

--- 05 aula - estrutura de repeticao (while and do while).cpp.cpp
+++ 05 aula - estrutura de repeticao (while and do while).cpp.cpp
@@ -108,17 +108,15 @@ int main() {
 
 /*
 
-Quando usar do while:
-
-Use a estrutura do while quando você deseja garantir que o bloco de código seja executado pelo menos uma vez, independentemente da condição.
-É útil em situações onde a primeira execução deve ocorrer antes da verificação da condição. 
-
---
-
 Quando usar while:
 
 Use a estrutura while quando a quantidade de iterações não for conhecida de antemão e depende de uma condição a ser satisfeita.
 É ideal quando você precisa verificar a condição antes de executar o bloco de código.
+
+Quando usar do while:
+
+Use a estrutura do while quando você deseja garantir que o bloco de código seja executado pelo menos uma vez, independentemente da condição.
+É útil em situações onde a primeira execução deve ocorrer antes da verificação da condição. 
 
 */
 
